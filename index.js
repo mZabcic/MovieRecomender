282327efec40469dfee8fa7f11ae767cc7740bae
Mislav <mislav.zabcic@gmail.com>
2018-12-19 12:22:36 +0000

    Production

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var app = require('./app');
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+app.listen(8080, () => {
+    console.log("Server running on port 8080");
    });
