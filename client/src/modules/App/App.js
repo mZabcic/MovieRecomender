f59ce6b17393fe96533459a47a4deb1b25d7c60f
Peggia <juraj.pejnovic@gmail.com>
2019-01-13 15:57:45 +0100

    add App.js remove config

---
+++ client/src/modules/App/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Home } from 'modules/pages/Home';
+import { Profile } from "modules/pages/Profile";
+import { About } from "modules/pages/About";
+import { Login } from "modules/pages/Login";
+import { Contact } from "modules/pages/Contact";
+import { TopMovies } from "modules/pages/TopMovies";
+import { MyMovies } from 'modules/pages/MyMovies';
+import { PrivateRoute } from "modules/components";
+import { Router, Route, Switch } from "react-router-dom";
+import { history } from "modules/services";
+
+export default function App(props) {
+  return (
+    <Router history={history} forceRefresh={true}>
+      <Switch>
+        <PrivateRoute path="/profile" component={Profile} />
+        <PrivateRoute path="/about" component={About} />
+        <PrivateRoute path="/contact" component={Contact} />
+        <PrivateRoute path="/top-movies" component={TopMovies} />
+        <PrivateRoute path="/my-movies" component={MyMovies} />
+        <Route path="/login" component={Login} />
+        <PrivateRoute path="/" component={Home} />
+      </Switch>
+    </Router>
+  );
+}
