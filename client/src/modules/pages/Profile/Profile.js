fcfdeaf3aa962394e40519ac9e579191a9d4adc2
bunda <ante.bundovic@zeraxo.com>
2019-01-14 07:03:29 +0100

    profile page

--- client/src/modules/pages/Profile/Profile.js
+++ client/src/modules/pages/Profile/Profile.js
@@ -2,7 +2,7 @@ import React, { PureComponent } from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import { Page } from "modules/components";
-import { logoutUser, getUser } from "modules/redux";
+import { logoutUser, getUser, fetchUser } from "modules/redux";
 
 class Profile extends PureComponent {
   constructor(props) {
@@ -10,6 +10,10 @@ class Profile extends PureComponent {
 
     this.handleLogoutClick = this.handleLogoutClick.bind(this);
 
+  }
+
+  componentDidMount() {
+    this.props.fetchUserAction();
   }
 
   handleLogoutClick() {
@@ -25,16 +29,28 @@ class Profile extends PureComponent {
         onLogoutClick={this.handleLogoutClick}
         loggedIn
         user={user}>
-        <div />
+        <ProfileCard user={this.props.user}/>
       </Page>
     );
   }
+}
+
+const ProfileCard = ({user}) => {
+  console.log({user})
+  const render = 
+    <div className="moviesHomeSection">
+      <div>Name: {user.first_name}</div>
+      <div>Surname: {user.last_name}</div>
+      <div>Email: {user.email}</div>
+      <div>No of my movies: {user.movies ? user.movies.length : 0}</div>
+    </div>;
+  return render;
 }
 
 function mapStateToProps(state) {
   return { user: getUser(state) };
 }
 
-const mapDispatchToProps = { logoutUserAction: logoutUser };
+const mapDispatchToProps = { logoutUserAction: logoutUser, fetchUserAction: fetchUser };
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Profile));
