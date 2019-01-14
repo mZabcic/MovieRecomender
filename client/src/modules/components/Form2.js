ce03a08e32a00e5132db5e03640dd47d6ad149ac
Ivan Ivkosic <ivan.ivkosic@gmail.com>
2019-01-14 01:47:03 +0100

    ante bundovic me zamolio 35 puta da pusham

---
+++ client/src/modules/components/Form2.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+class Form2 extends React.Component {
+
+	state = {
+  		tracks : []
+  	}
+
+
+	getArtists = async () => {   
+	    const api_call = await fetch(`http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=b02f4ce77743619481b799a16fca4cd9&format=json&limit=10`);
+	    const data = await api_call.json(); 
+	  	this.setState ( { 
+	  		tracks : data.tracks.track,
+	    });
+   	}
+
+  	createTable =  () => {
+  		let table = []     	    
+
+	    let len = this.state.tracks.length;
+	    if(len>10) len = 10;
+	    if(len > 0 ) {
+	    for (let i = 0; i < len; i++) { 
+	      //Create the parent and add the children
+	      table.push(<p>{this.state.tracks[i].name}</p>)
+	    }
+		}
+	    return table
+ 	}
+
+
+ 	componentDidMount() {
+		this.getArtists();
+	}
+	
+	render() { 
+		return ( 
+			<div className="greySection">
+			<p className="artistsTitle">TOP 10 tracks according to <b>last.fm</b></p>
+			<hr/>
+				{this.createTable()}
+			</div>
+		);
+	}
+}
+
+export default Form2;
---
+++ client/src/modules/components/Weather.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+const bearer_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YzNiMjk3NjA0MDY5MTMwNzQ1ZjhlNWEiLCJlbWFpbCI6Im1pc2xhdi56YWJjaWNAZ21haWwuY29tIiwicm9sZSI6IkFkbWluIiwic3ViIjoiNWMzYjI5NzYwNDA2OTEzMDc0NWY4ZTVhIiwiaWF0IjoxNTQ3Mzg3MzE4LCJleHAiOjE1NDc0NzM3MTgsImp0aSI6InN1YiJ9.bue1DUDeHR0j11mR7lIGq0XDLrLtHwIT81T-hIf-fQ0";
+
+class Weather extends React.Component {
+
+	state = {
+	    temperature : undefined,
+	    city : undefined,
+	    country : undefined,
+	    humidity : undefined,
+	    description : undefined,
+	    refreshDate: undefined,
+	    error: undefined,
+  	}
+
+
+
+	getWeather = async () => { 
+	    var bearer = 'Bearer '+ bearer_token;
+	    const api_call = await fetch(`http://165.227.128.66/v1/weather`,{
+		  headers: {
+		    'Authorization': bearer
+		   }
+		});
+	    const data = await api_call.json(); 
+	   	var h = new Date(data.dt*1000).getHours();
+	   	var m = new Date(data.dt*1000).getMinutes(); 
+	   	var rd = h + ":" + m; 
+	    this.setState ( {
+	      temperature : "Temperature " + data.main.temp + " C",
+	      city : "City: " + data.name,
+	      country : "Country: " + data.sys.country,
+	      humidity : "Humidity: " + data.main.humidity + " %",
+	      description : "Sky: " + data.weather[0].description,
+	      refreshDate : "Last refreshed: " + rd,
+	      error : ""
+	    });
+	}
+ 
+
+	componentDidMount() {
+		this.getWeather();
+	}
+	
+	render() {
+		this.getWeather()
+		return (
+			<div className="weatherSection ">
+			<img alt="none" className="weatherImg" src={window.location.origin + '/img/foreca.png'}></img>
+				<hr/>
+				<p>{this.state.country}</p>
+				<p>{this.state.city}</p> 
+				<p>{this.state.description}</p>
+				<p>{this.state.temperature}</p>
+				<p>{this.state.humidity}</p>	
+				<p>{this.state.refreshDate}</p>							
+			</div>
+		);
+	}
+}
+
+export default Weather;
+
